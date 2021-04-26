1397b5969fde5f9e73e35818d2fb1d13c7cfc3bb
hcbarros <48363350+hcbarros@users.noreply.github.com>
2021-04-26 01:32:56 -0300

    commit

--- src/test/java/com/api/test/ApiTestApplicationTests.java
+++ src/test/java/com/api/test/ApiTestApplicationTests.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.time.LocalDateTime;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -34,40 +37,33 @@ class ApiTestApplicationTests {
 	
 	private RestTemplate restTemplate;
 	
-	private HttpHeaders headers; 
-	
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	
+	private HttpHeaders headers;
+	
 	private JSONObject obj;
+	
 	
 	@BeforeEach
 	public void runBeforeAllTestMethods() {
 		restTemplate = new RestTemplate();
 		obj = new JSONObject();
+		headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 	}
 
 	@Order(1)
 	@Test
 	public void shouldCreateUser() throws JsonMappingException, JsonProcessingException {
 	
-//		JSONParser jsonParser = new JSONParser();
-//		JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader("data.json"));
-//
-//		Iterator<JSONObject> iterator = jsonArray.iterator();
-//		JSONObject obj = (JSONObject) iterator.next();
-
-				
 		obj.put("id", 1);
-		obj.put("username", "Chiquinha");
-		obj.put("firstname", "Silva");
-		obj.put("lastname", "Pereira");
-		obj.put("email", "chiquinha@gmail.com");
-		obj.put("password", "chiquinha123");
+		obj.put("username", "massuncao");
+		obj.put("firstname", "Maria");
+		obj.put("lastname", "Assunção");
+		obj.put("email", "mariassun@gmail.com");
+		obj.put("password", "massun123");
 		obj.put("phone", "999999999");
 		obj.put("userStatus", 0);
-		
-		HttpHeaders headers = headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
 		
 		HttpEntity<String> request =
 			new HttpEntity<String>(obj.toString(), headers);
@@ -78,45 +74,117 @@ class ApiTestApplicationTests {
 		
 		JsonNode root = objectMapper.readTree(responseEntityStr.getBody());
 		
-		assertNotNull(responseEntityStr.getBody());
+		assertEquals(responseEntityStr.getStatusCodeValue(), 200);
 	    assertNotNull(root.path("username").asText());
 	}
 
 	@Order(2)
 	@Test
 	public void shouldCreatePet() throws JsonMappingException, JsonProcessingException {
-	
-//		JSONParser jsonParser = new JSONParser();
-//		JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader("data.json"));
-//
-//		Iterator<JSONObject> iterator = jsonArray.iterator();
-//		JSONObject obj = (JSONObject) iterator.next();
 
+		JSONObject category = new JSONObject();
+		category.put("id", 1);
+		category.put("name", "dog");
+		JSONObject tag = new JSONObject();
+		tag.put("id", 1);
+		tag.put("name", "tag1");
+		
+		JSONArray arrayImg = new JSONArray();
+		arrayImg.add("https://images.dog.ceo/breeds/akita/Akita_Inu_dog.jpg");
+		JSONArray arrayTags = new JSONArray();
+		arrayTags.add(tag);
+		
+		obj.put("id", 1);
+		obj.put("category", category);		
+		obj.put("name", "Brutus");
+		obj.put("photoUrls", arrayImg);
+		obj.put("tags", arrayTags);
+		obj.put("status", "available");
+	
+		HttpEntity<String> request =
+			new HttpEntity<String>(obj.toString(), headers);
+		
+		ResponseEntity<String> responseEntityStr = 
+			      restTemplate.postForEntity("https://petstore.swagger.io/v2/pet",
+			    		  request, String.class);
+		
+		JsonNode root = objectMapper.readTree(responseEntityStr.getBody());
+		
+		assertEquals(responseEntityStr.getStatusCodeValue(), 200);
+		assertEquals(root.get("name").asText(), "Brutus");
+	}
+	
+	
+	@Order(3)
+	@Test
+	public void shouldSellPet() throws JsonMappingException, JsonProcessingException {
 				
 		obj.put("id", 1);
-		obj.put("username", "Chiquinha");
-		obj.put("firstname", "Silva");
-		obj.put("lastname", "Pereira");
-		obj.put("email", "chiquinha@gmail.com");
-		obj.put("password", "chiquinha123");
-		obj.put("phone", "999999999");
-		obj.put("userStatus", 0);
-		
-		HttpHeaders headers = headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+		obj.put("petId", 1);
+		obj.put("quantity", 1);
+		obj.put("shipDate", LocalDateTime.now().toString());
+		obj.put("status", "placed");
+		obj.put("complete", true);
 		
 		HttpEntity<String> request =
 			new HttpEntity<String>(obj.toString(), headers);
 		
 		ResponseEntity<String> responseEntityStr = 
-			      restTemplate.postForEntity("https://petstore.swagger.io/v2/user/",
+			      restTemplate.postForEntity("https://petstore.swagger.io/v2/store/order",
 			    		  request, String.class);
 		
 		JsonNode root = objectMapper.readTree(responseEntityStr.getBody());
 		
-		assertNotNull(responseEntityStr.getBody());
-	    assertNotNull(root.path("username").asText());
+		assertEquals(responseEntityStr.getStatusCodeValue(), 200);
+	    assertEquals(root.get("status").asText(), "placed");
+	    assertEquals(root.get("petId").asInt(), 1);
 	}
+	
+	@Order(4)
+	@Test
+	public void shouldChangeOrder() throws JsonMappingException, JsonProcessingException {
+				
+		ResponseEntity<String> order1 = 
+			      restTemplate.getForEntity("https://petstore.swagger.io/v2/store/order/1",
+			    		   String.class);
+		
+		JsonNode root = objectMapper.readTree(order1.getBody());
+		
+		obj.put("id", root.get("id").asInt());
+		obj.put("petId", root.get("petId").asInt());
+		obj.put("quantity", root.get("quantity").asInt());
+		obj.put("shipDate", root.get("shipDate").asText());
+		obj.put("status", "delivered");
+		obj.put("complete", true);
+		
+		HttpEntity<String> request =
+			new HttpEntity<String>(obj.toString(), headers);
+		
+		ResponseEntity<String> responseEntityStr = 
+			      restTemplate.postForEntity("https://petstore.swagger.io/v2/store/order",
+			    		  request, String.class);
+		
+		root = objectMapper.readTree(responseEntityStr.getBody());
+		
+		assertEquals(responseEntityStr.getStatusCodeValue(), 200);
+	    assertEquals(root.get("status").asText(), "delivered");
+	}
+	
+	@Order(5)
+	@Test
+	public void shouldToValidChangedOrder() throws JsonMappingException, JsonProcessingException {
+				
+		ResponseEntity<String> order1 = 
+			      restTemplate.getForEntity("https://petstore.swagger.io/v2/store/order/1",
+			    		   String.class);
+		
+		JsonNode root = objectMapper.readTree(order1.getBody());
+		
+		assertEquals(order1.getStatusCodeValue(), 200);
+	    assertEquals(root.get("status").asText(), "delivered");
+	}
+	
+	
 
 	
 }
